b988fcaf77b97fed9ace7d8a1c67e8560710ec75
Juan-Rpenuela <juan.rpenuela@mail.escuelaing.edu.co>
2025-02-12 09:10:44 -0500

    chore: initialize backscaffolding for ReservasLab

---
+++ src/main/java/edu/eci/cvds/reservas/ReservasLabApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/reservas/ReservasLabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
